fd89f49633f880bb215a4d0c30ec8176a026cc27
Serhiy Brytskyy <sbrytskyy@google.com>
2019-04-18 17:47:47 -0700

    Removed unnecessary project

--- Algorithms/src/PercolationStats.java
+++ Algorithms/src/PercolationStats.java
@@ -3,10 +3,12 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-    
+
     private static final double CONFIDENCE_95 = 1.96;
     private final double[] pts;
-    
+    private double mean = Double.NaN;
+    private double stddev = Double.NaN;
+
     public PercolationStats(int n, int trials) { // perform trials independent
                                                  // experiments on an n-by-n
                                                  // grid
@@ -38,29 +40,32 @@ public class PercolationStats {
     }
 
     public double mean() { // sample mean of percolation threshold
-        return StdStats.mean(pts);
+        if (Double.isNaN(mean)) {
+            mean = StdStats.mean(pts);
+        }
+        return mean;
     }
 
     public double stddev() { // sample standard deviation of percolation
                              // threshold
-        return StdStats.stddev(pts);
+        if (Double.isNaN(stddev)) {
+            stddev = StdStats.stddev(pts);
+        }
+        return stddev;
     }
 
     public double confidenceLo() { // low endpoint of 95% confidence interval
-        return StdStats.mean(pts) - CONFIDENCE_95 * StdStats.stddev(pts) / Math.sqrt(pts.length);
+        return mean() - CONFIDENCE_95 * stddev() / Math.sqrt(pts.length);
     }
 
     public double confidenceHi() { // high endpoint of 95% confidence interval
-        return StdStats.mean(pts) + CONFIDENCE_95 * StdStats.stddev(pts) / Math.sqrt(pts.length);
+        return mean() + CONFIDENCE_95 * stddev() / Math.sqrt(pts.length);
     }
 
     public static void main(String[] args) {
-        int n = 20; 
-        int trials = 100;
-        if (args.length == 2) {
-            n = Integer.parseInt(args[0]);
-            trials = Integer.parseInt(args[1]);
-        }
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+
         PercolationStats ps = new PercolationStats(n, trials);
         StdOut.println(ps.mean());
         StdOut.println(ps.stddev());
