00e24f6beebb5ff3b776998871c091e23df23564
Serhiy Brytskyy <sbrytskyy@gmail.com>
2019-04-23 22:37:53 -0700

    91%

--- Algorithms/src/FastCollinearPoints.java
+++ Algorithms/src/FastCollinearPoints.java
@@ -3,103 +3,102 @@ import java.util.Collections;
 import java.util.List;
 
 public class FastCollinearPoints {
-    
-    private List<LineSegment> arr = new ArrayList<>();
-    
-    private class Item implements Comparable<Item> {
-        
-        private double slope;
-        private Point p;
-        
-        public int compareTo(Item that) {
-            if (slope > that.slope) {
-                return 1;
-            } else if (slope < that.slope) {
-                return -1;
-            }
-            return 0;
-        }
-    }
 
+	private List<LineSegment> arr = new ArrayList<>();
 
-    public FastCollinearPoints(Point[] points) { // finds all line segments
-                                                 // containing 4 or more points
+	private class Item implements Comparable<Item> {
 
-        if (points == null) {
-            throw new NullPointerException();
-        }
+		private double slope;
+		private Point p;
 
-        int len = points.length;
+		public int compareTo(Item that) {
+			if (slope > that.slope) {
+				return 1;
+			} else if (slope < that.slope) {
+				return -1;
+			}
+			return 0;
+		}
+	}
 
-        for (int ip = 0; ip < len - 1; ip++) {
-            Point p = points[ip];
-            if (p == null) {
-                throw new NullPointerException();
-            }
-            
-            List<Item> items = new ArrayList<>();
-            
-            for (int iq = ip + 1; iq < len; iq++) {
-                Point q = points[iq];
-                if (q == null) {
-                    throw new NullPointerException();
-                }
-                if (q.compareTo(p) == 0) {
-                    throw new IllegalArgumentException();
-                }
-                
-                double slopePQ = p.slopeTo(q);
-                Item item = new Item();
-                item.slope = slopePQ;
-                item.p = q;
-                items.add(item);
-            }
-            Collections.sort(items);
+	public FastCollinearPoints(Point[] points) { // finds all line segments
+													// containing 4 or more points
 
-            List<Point> l = new ArrayList<>();
+		if (points == null) {
+			throw new NullPointerException();
+		}
 
-            int counter = 0;
-            for (int i = 1; i < items.size(); i++) {
-                Item itemPrev = items.get(i-1);
-                Item item = items.get(i);
-                if (Double.compare(item.slope, itemPrev.slope) == 0) {
-                    if (l.isEmpty()) {
-                        l.add(itemPrev.p);
-                        counter++;
-                    }
-                    l.add(item.p);
-                    counter++;
-                } else {
-                    if (counter >= 3) {
-                        l.add(p);
-                        LineSegment seg = createSegment(l);
-                        arr.add(seg);
-                    }
-                    counter = 0;
-                    l = new ArrayList<>();
-                }
-            }
-            if (counter >= 3) {
-                l.add(p);
-                LineSegment seg = createSegment(l);
-                arr.add(seg);
-            }
-        }
-    }
+		int len = points.length;
 
-    private LineSegment createSegment(List<Point> l) {
-        Collections.sort(l);
-        Point p1 = l.get(0);
-        Point p2 = l.get(l.size() - 1);
+		for (int ip = 0; ip < len - 1; ip++) {
+			Point p = points[ip];
+			if (p == null) {
+				throw new IllegalArgumentException();
+			}
 
-        return new LineSegment(p1, p2);
-    }
+			List<Item> items = new ArrayList<>();
 
-    public int numberOfSegments() { // the number of line segments
-        return arr.size();
-    }
+			for (int iq = ip + 1; iq < len; iq++) {
+				Point q = points[iq];
+				if (q == null) {
+					throw new NullPointerException();
+				}
+				if (q.compareTo(p) == 0) {
+					throw new IllegalArgumentException();
+				}
 
-    public LineSegment[] segments() { // the line segments
-        return arr.toArray(new LineSegment[arr.size()]);
-    }
+				double slopePQ = p.slopeTo(q);
+				Item item = new Item();
+				item.slope = slopePQ;
+				item.p = q;
+				items.add(item);
+			}
+			Collections.sort(items);
+
+			List<Point> l = new ArrayList<>();
+
+			int counter = 0;
+			for (int i = 1; i < items.size(); i++) {
+				Item itemPrev = items.get(i - 1);
+				Item item = items.get(i);
+				if (Double.compare(item.slope, itemPrev.slope) == 0) {
+					if (l.isEmpty()) {
+						l.add(itemPrev.p);
+						counter++;
+					}
+					l.add(item.p);
+					counter++;
+				} else {
+					if (counter >= 3) {
+						l.add(p);
+						LineSegment seg = createSegment(l);
+						arr.add(seg);
+					}
+					counter = 0;
+					l = new ArrayList<>();
+				}
+			}
+			if (counter >= 3) {
+				l.add(p);
+				LineSegment seg = createSegment(l);
+				arr.add(seg);
+			}
+		}
+	}
+
+	private LineSegment createSegment(List<Point> l) {
+		Collections.sort(l);
+		Point p1 = l.get(0);
+		Point p2 = l.get(l.size() - 1);
+
+		return new LineSegment(p1, p2);
+	}
+
+	public int numberOfSegments() { // the number of line segments
+		return arr.size();
+	}
+
+	public LineSegment[] segments() { // the line segments
+		return arr.toArray(new LineSegment[arr.size()]);
+	}
 }
--- Algorithms/src/Point.java
+++ Algorithms/src/Point.java
@@ -19,154 +19,150 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class Point implements Comparable<Point> {
 
-    private final int x; // x-coordinate of this point
-    private final int y; // y-coordinate of this point
+	private final int x; // x-coordinate of this point
+	private final int y; // y-coordinate of this point
 
-    /**
-     * Initializes a new point.
-     *
-     * @param x
-     *            the <em>x</em>-coordinate of the point
-     * @param y
-     *            the <em>y</em>-coordinate of the point
-     */
-    public Point(int x, int y) {
-        /* DO NOT MODIFY */
-        this.x = x;
-        this.y = y;
-    }
+	/**
+	 * Initializes a new point.
+	 *
+	 * @param  x the <em>x</em>-coordinate of the point
+	 * @param  y the <em>y</em>-coordinate of the point
+	 */
+	public Point(int x, int y) {
+		/* DO NOT MODIFY */
+		this.x = x;
+		this.y = y;
+	}
 
-    /**
-     * Draws this point to standard draw.
-     */
-    public void draw() {
-        /* DO NOT MODIFY */
-        StdDraw.point(x, y);
-    }
+	/**
+	 * Draws this point to standard draw.
+	 */
+	public void draw() {
+		/* DO NOT MODIFY */
+		StdDraw.point(x, y);
+	}
 
-    /**
-     * Draws the line segment between this point and the specified point to
-     * standard draw.
-     *
-     * @param that
-     *            the other point
-     */
-    public void drawTo(Point that) {
-        /* DO NOT MODIFY */
-        StdDraw.line(this.x, this.y, that.x, that.y);
-    }
+	/**
+	 * Draws the line segment between this point and the specified point
+	 * to standard draw.
+	 *
+	 * @param that the other point
+	 */
+	public void drawTo(Point that) {
+		/* DO NOT MODIFY */
+		StdDraw.line(this.x, this.y, that.x, that.y);
+	}
 
-    /**
-     * Returns the slope between this point and the specified point. Formally,
-     * if the two points are (x0, y0) and (x1, y1), then the slope is (y1 - y0)
-     * / (x1 - x0). For completeness, the slope is defined to be +0.0 if the
-     * line segment connecting the two points is horizontal;
-     * Double.POSITIVE_INFINITY if the line segment is vertical; and
-     * Double.NEGATIVE_INFINITY if (x0, y0) and (x1, y1) are equal.
-     *
-     * @param that
-     *            the other point
-     * @return the slope between this point and the specified point
-     */
-    public double slopeTo(Point that) {
-        if (that.x == x) {
-            if (that.y == y) {
-                return Double.NEGATIVE_INFINITY;
-            }
-            return Double.POSITIVE_INFINITY;
-        }
-        if (that.y == y) {
-            return 0.0;
-        }
-        return 1.0 * (that.y - y) / (that.x - x);
-    }
+	/**
+	 * Returns the slope between this point and the specified point.
+	 * Formally, if the two points are (x0, y0) and (x1, y1), then the slope
+	 * is (y1 - y0) / (x1 - x0). For completeness, the slope is defined to be
+	 * +0.0 if the line segment connecting the two points is horizontal;
+	 * Double.POSITIVE_INFINITY if the line segment is vertical;
+	 * and Double.NEGATIVE_INFINITY if (x0, y0) and (x1, y1) are equal.
+	 *
+	 * @param  that the other point
+	 * @return the slope between this point and the specified point
+	 */
+	public double slopeTo(Point that) {
+		if (that.x == x) {
+			if (that.y == y) {
+				return Double.NEGATIVE_INFINITY;
+			}
+			return Double.POSITIVE_INFINITY;
+		}
+		if (that.y == y) {
+			return 0.0;
+		}
+		return 1.0 * (that.y - y) / (that.x - x);
+	}
 
-    /**
-     * Compares two points by y-coordinate, breaking ties by x-coordinate.
-     * Formally, the invoking point (x0, y0) is less than the argument point
-     * (x1, y1) if and only if either y0 < y1 or if y0 = y1 and x0 < x1.
-     *
-     * @param that
-     *            the other point
-     * @return the value <tt>0</tt> if this point is equal to the argument point
-     *         (x0 = x1 and y0 = y1); a negative integer if this point is less
-     *         than the argument point; and a positive integer if this point is
-     *         greater than the argument point
-     */
-    public int compareTo(Point that) {
-        if (y > that.y) {
-            return 1;
-        } else if (y < that.y) {
-            return -1;
-        } else if (x > that.x) {
-            return 1;
-        } else if (x < that.x) {
-            return -1;
-        }
-        return 0;
-    }
+	/**
+	 * Compares two points by y-coordinate, breaking ties by x-coordinate.
+	 * Formally, the invoking point (x0, y0) is less than the argument point
+	 * (x1, y1) if and only if either y0 < y1 or if y0 = y1 and x0 < x1.
+	 *
+	 * @param  that the other point
+	 * @return the value <tt>0</tt> if this point is equal to the argument
+	 *         point (x0 = x1 and y0 = y1);
+	 *         a negative integer if this point is less than the argument
+	 *         point; and a positive integer if this point is greater than the
+	 *         argument point
+	 */
+	public int compareTo(Point that) {
+		if (y > that.y) {
+			return 1;
+		} else if (y < that.y) {
+			return -1;
+		} else if (x > that.x) {
+			return 1;
+		} else if (x < that.x) {
+			return -1;
+		}
+		return 0;
+	}
 
-    /**
-     * Compares two points by the slope they make with this point. The slope is
-     * defined as in the slopeTo() method.
-     *
-     * @return the Comparator that defines this ordering on points
-     */
-    public Comparator<Point> slopeOrder() {
-        
-        final Point p0 = this; 
-        return new Comparator<Point>() {
+	/**
+	 * Compares two points by the slope they make with this point.
+	 * The slope is defined as in the slopeTo() method.
+	 *
+	 * @return the Comparator that defines this ordering on points
+	 */
+	public Comparator<Point> slopeOrder() {
 
-            @Override
-            public int compare(Point p1, Point p2) {
-                return Double.compare(p1.slopeTo(p0), p2.slopeTo(p0));
-            }
-        };
-    }
+		final Point p0 = this;
+		return new Comparator<Point>() {
 
-    /**
-     * Returns a string representation of this point. This method is provide for
-     * debugging; your program should not rely on the format of the string
-     * representation.
-     *
-     * @return a string representation of this point
-     */
-    public String toString() {
-        /* DO NOT MODIFY */
-        return "(" + x + ", " + y + ")";
-    }
+			@Override
+			public int compare(Point p1, Point p2) {
+				return Double.compare(p1.slopeTo(p0), p2.slopeTo(p0));
+			}
+		};
+	}
 
-    /**
-     * Unit tests the Point data type.
-     */
-    public static void main(String[] args) {
+	/**
+	 * Returns a string representation of this point.
+	 * This method is provide for debugging;
+	 * your program should not rely on the format of the string representation.
+	 *
+	 * @return a string representation of this point
+	 */
+	public String toString() {
+		/* DO NOT MODIFY */
+		return "(" + x + ", " + y + ")";
+	}
 
-        // read the n points from a file
-        In in = new In(args[0]);
-        int n = in.readInt();
-//        int n = 100;
-//        Random r = new Random();
-                
-        Point[] points = new Point[n];
-        for (int i = 0; i < n; i++) {
-            int x = in.readInt();
-            int y = in.readInt();
-//          int x = r.nextInt(32);
-//          int y = r.nextInt(32);
-            points[i] = new Point(x, y);
-        }
-        
-        BruteCollinearPoints b = new BruteCollinearPoints(points);
-        StdOut.println(b.segments().length);
-        for (LineSegment segment : b.segments()) {
-            StdOut.println(segment);
-        }
+	/**
+	 * Unit tests the Point data type.
+	 */
+	public static void main(String[] args) {
 
-        FastCollinearPoints f = new FastCollinearPoints(points);
-        StdOut.println(f.segments().length);
-        for (LineSegment segment : f.segments()) {
-            StdOut.println(segment);
-        }
+		// read the n points from a file
+		In in = new In(args[0]);
+		int n = in.readInt();
+		//        int n = 100;
+		//        Random r = new Random();
 
-    }
+		Point[] points = new Point[n];
+		for (int i = 0; i < n; i++) {
+			int x = in.readInt();
+			int y = in.readInt();
+			//          int x = r.nextInt(32);
+			//          int y = r.nextInt(32);
+			points[i] = new Point(x, y);
+		}
+
+		BruteCollinearPoints b = new BruteCollinearPoints(points);
+		StdOut.println(b.segments().length);
+		for (LineSegment segment : b.segments()) {
+			StdOut.println(segment);
+		}
+
+		FastCollinearPoints f = new FastCollinearPoints(points);
+		StdOut.println(f.segments().length);
+		for (LineSegment segment : f.segments()) {
+			StdOut.println(segment);
+		}
+
+	}
 }
