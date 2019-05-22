47fcf67a9ae8f1157c7e213be614fea3407f7212
Serhiy Brytskyy <sbrytskyy@gmail.com>
2019-05-21 21:23:12 -0700

    Added skeleton

---
+++ Algorithms2/src/BaseballElimination.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class BaseballElimination {
+
+    public BaseballElimination(String filename) { // create a baseball division from given filename in format specified below
+
+    }
+
+    public int numberOfTeams() { // number of teams
+        return 0;
+    }
+
+    public Iterable<String> teams() { // all teams
+        return new ArrayList<>();
+    }
+
+    public int wins(String team) { // number of wins for given team
+        return 0;
+    }
+
+    public int losses(String team) { // number of losses for given team
+        return 0;
+    }
+
+    public int remaining(String team) { // number of remaining games for given team
+        return 0;
+    }
+
+    public int against(String team1, String team2) { // number of remaining games between team1 and team2
+        return 0;
+    }
+
+    public boolean isEliminated(String team) { // is given team eliminated?
+        return false;
+    }
+
+    public Iterable<String> certificateOfElimination(String team) { // subset R of teams that eliminates given team; null if not eliminated
+        return null;
+    }
+
+    public static void main(String[] args) {
+        BaseballElimination division = new BaseballElimination(args[0]);
+        for (String team : division.teams()) {
+            if (division.isEliminated(team)) {
+                StdOut.print(team + " is eliminated by the subset R = { ");
+                for (String t : division.certificateOfElimination(team)) {
+                    StdOut.print(t + " ");
+                }
+                StdOut.println("}");
+            } else {
+                StdOut.println(team + " is not eliminated");
+            }
+        }
+    }
+}
