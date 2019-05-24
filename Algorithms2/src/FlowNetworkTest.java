357267b89808e3d13a2ad78f88c01f3dd1abf64f
Serhiy Brytskyy <sbrytskyy@google.com>
2019-05-24 16:52:10 -0700

    Added class for play with FlowNetwork

---
+++ Algorithms2/src/FlowNetworkTest.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.princeton.cs.algs4.FlowEdge;
+import edu.princeton.cs.algs4.FlowNetwork;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class FlowNetworkTest {
+
+    @Override
+    public String toString() {
+        return "FlowNetworkTest [source=" + source + ", sink=" + sink + ", fn=\n" + fn + "]\n";
+    }
+
+    private final FlowNetwork fn;
+    private final int source;
+    private final int sink;
+
+    public FlowNetworkTest(String filename) {
+        In in = new In(filename);
+        fn = new FlowNetwork(in);
+
+        source = 0;
+        sink = fn.V() - 1;
+    }
+
+    private void findMaxflow() {
+        dfs(source, new ArrayList<>());
+    }
+
+    private void dfs(int v, List<FlowEdge> path) {
+        if (v == sink) {
+            StdOut.println(path);
+            return;
+        }
+
+        Iterable<FlowEdge> adj = fn.adj(v);
+        for (FlowEdge e : adj) {
+            if (e.from() == v) {
+                path.add(e);
+                dfs(e.other(v), path);
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        test1("resources/test/flownet1.txt");
+    }
+
+    private static void test1(String filename) {
+        FlowNetworkTest fnt = new FlowNetworkTest(filename);
+        StdOut.println(fnt);
+        fnt.findMaxflow();
+    }
+
+}
