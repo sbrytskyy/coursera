25ca616c75995a21a6c04564bc51573da0b89abb
Serhiy Brytskyy <sbrytskyy@google.com>
2019-06-03 17:06:26 -0700

    63%

---
+++ Algorithms2/src/CircularSuffixArray.java
@@ -0,0 +1,18 @@
+
+public class CircularSuffixArray {
+    public CircularSuffixArray(String s) { // circular suffix array of s
+
+    }
+
+    public int length() { // length of s
+        return 0;
+    }
+
+    public int index(int i) { // returns index of ith sorted suffix
+        return i;
+    }
+
+    public static void main(String[] args) { // unit testing (required)
+
+    }
+}
--- Algorithms2/src/MoveToFront.java
+++ Algorithms2/src/MoveToFront.java
@@ -1,3 +1,6 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+
 import edu.princeton.cs.algs4.BinaryStdIn;
 import edu.princeton.cs.algs4.BinaryStdOut;
 
@@ -12,7 +15,25 @@ public class MoveToFront {
         String s = BinaryStdIn.readString();
         char[] input = s.toCharArray();
 
-//        BinaryStdOut.write(input.length);
+        LinkedList<Character> alphabet = new LinkedList<>();
+        for (char c = 0; c < R; c++) {
+            alphabet.add(c);
+        }
+
+        for (char c : input) {
+            byte pos = 0;
+            Iterator<Character> iterator = alphabet.iterator();
+            while (iterator.hasNext()) {
+                char next = iterator.next();
+                if (next == c) {
+                    iterator.remove();
+                    alphabet.addFirst(c);
+                    BinaryStdOut.write(pos);
+                    break;
+                }
+                pos++;
+            }
+        }
 
         // close output stream
         BinaryStdOut.close();
@@ -20,12 +41,39 @@ public class MoveToFront {
 
     // apply move-to-front decoding, reading from standard input and writing to standard output
     public static void decode() {
+        // read the input
+        String s = BinaryStdIn.readString();
+        char[] input = s.toCharArray();
 
+        LinkedList<Character> alphabet = new LinkedList<>();
+        for (char c = 0; c < R; c++) {
+            alphabet.add(c);
+        }
+
+        for (char c : input) {
+            int pos = c;
+            char ch = alphabet.remove(pos);
+            alphabet.addFirst(ch);
+            BinaryStdOut.write(ch);
+        }
+
+        // close output stream
+        BinaryStdOut.close();
     }
 
     // if args[0] is '-', apply move-to-front encoding
     // if args[0] is '+', apply move-to-front decoding
     public static void main(String[] args) {
+        if (args.length < 1) {
+            return;
+        }
 
+        if ("-".equals(args[0])) {
+            encode();
+        }
+
+        if ("+".equals(args[0])) {
+            decode();
+        }
     }
 }
