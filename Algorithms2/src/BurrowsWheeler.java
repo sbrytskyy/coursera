737746f67f58beee254ce6e29ae986a4992e68ba
Serhiy Brytskyy <sbrytskyy@google.com>
2019-06-05 12:33:05 -0700

    99%

--- Algorithms2/src/BurrowsWheeler.java
+++ Algorithms2/src/BurrowsWheeler.java
@@ -40,9 +40,9 @@ public class BurrowsWheeler {
         int first = BinaryStdIn.readInt();
         String encoded = BinaryStdIn.readString();
         char[] t = encoded.toCharArray();
-        
+
         int len = t.length;
-        
+
         Map<Character, LinkedList<Integer>> map = new LinkedHashMap<>();
         for (int i = 0; i < len; i++) {
             LinkedList<Integer> linkedList = map.getOrDefault(t[i], new LinkedList<>());
@@ -50,19 +50,53 @@ public class BurrowsWheeler {
             map.put(t[i], linkedList);
         }
 
-        Arrays.sort(t);
+        countingSort(t);
         int next[] = new int[len];
         for (int i = 0; i < len; i++) {
             next[i] = map.get(t[i]).remove();
         }
-        
+
         for (int i = 0; i < len; i++) {
             BinaryStdOut.write(t[first]);
             first = next[first];
         }
-        
+
         // close output stream
         BinaryStdOut.close();
+    }
+
+    private static void countingSort(char arr[]) {
+        int n = arr.length;
+
+        // The output character array that will have sorted arr 
+        char output[] = new char[n];
+
+        // Create a count array to store count of inidividul 
+        // characters and initialize count array as 0 
+        int count[] = new int[256];
+        for (int i = 0; i < 256; ++i)
+            count[i] = 0;
+
+        // store count of each character 
+        for (int i = 0; i < n; ++i)
+            ++count[arr[i]];
+
+        // Change count[i] so that count[i] now contains actual 
+        // position of this character in output array 
+        for (int i = 1; i <= 255; ++i)
+            count[i] += count[i - 1];
+
+        // Build the output character array 
+        // To make it stable we are operating in reverse order. 
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[arr[i]] - 1] = arr[i];
+            --count[arr[i]];
+        }
+
+        // Copy the output array to arr, so that arr now 
+        // contains sorted characters 
+        for (int i = 0; i < n; ++i)
+            arr[i] = output[i];
     }
 
     // if args[0] is '-', apply Burrows-Wheeler transform
